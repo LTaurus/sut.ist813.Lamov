a5dadb06c611ffffb1b94f7fd81bbc623f8bdabc
LTaurus <lamovvladik@gmail.com>
2020-12-05 11:50:26 +0300

    first commit

---
+++ test_maven_project/src/test/java/sut/ist813/Lamov/AppTest.java
@@ -0,0 +1,20 @@
+package sut.ist813.Lamov;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
